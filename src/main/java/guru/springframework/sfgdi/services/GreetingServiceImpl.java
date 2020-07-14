4c599b56614824c6d0ce6075ae5747a305044018
Dustin Wright <nzeroxpd@gmail.com>
2020-07-14 12:54:56 -0500

    Video 35.

--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by DW 07/13/20
  */
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
